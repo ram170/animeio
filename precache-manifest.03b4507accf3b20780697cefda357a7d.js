9391d901edf5c8e19f3496cf94c65c5b6f30896d
Sriram <46366477+ram170@users.noreply.github.com>
2020-09-26 13:56:19 +0530

    Updates

---
+++ precache-manifest.03b4507accf3b20780697cefda357a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1aea3ef4997f0aaac54f83b25a8deba",
+    "url": "/animeio/index.html"
+  },
+  {
+    "revision": "09788411e3c6023fae0d",
+    "url": "/animeio/static/css/main.111cead8.chunk.css"
+  },
+  {
+    "revision": "6b1504d7e710062b16f8",
+    "url": "/animeio/static/js/2.d6e5c3d5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/animeio/static/js/2.d6e5c3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09788411e3c6023fae0d",
+    "url": "/animeio/static/js/main.ab029453.chunk.js"
+  },
+  {
+    "revision": "ac5c36e82108f8fb0cb9",
+    "url": "/animeio/static/js/runtime-main.d2848321.js"
+  }
+]);
